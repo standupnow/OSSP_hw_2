41903eb4fecddf74ba8875af82ecf689139c3e83
standupnow <sysilove1526@gmail.com>
2024-03-27 14:18:08 +0900

    2024-03-20 OSSP 개인과제#2

---
+++ 소스.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	string name;
+	int id;
+
+	cout << "이름을 입력하세요: ";
+	cin >> name;
+	cout << "학번을 입력하세요: ";
+	cin >> id;
+
+	cout << "<출력>\n";
+	cout << "이름: " << name << endl;
+	cout << "학번: " << id;
+}
+
